52ebd8fac4d2f2dba516e591f762397c6660e18c
CodeWithYaseen <yaseenbatch16@gmail.com>
2025-07-30 20:39:23 +0500

    JavaScript Assignment 3

--- Chapter 9/app.js
+++ Chapter 9/app.js
@@ -30,15 +30,12 @@
 // Q#7
 
 // var educationQualification = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil","PHD"];
-
 // console.log(educationQualification);
 
 // Q#8
 
 // studentsName = ["Yaseen","Hassan","Ali"];
-
 // var score = [420, 290,480];
-
 // var totalMarks = 500;
 
 // console.log("Score of",studentsName[0], "is" ,score[0]+".","Percentage: "+ (score[0]*100)/500+"%");
@@ -50,35 +47,35 @@
 // var colorName =["orange","purple"];
 // console.log(colorName);
 
-// part (a)
+// Q#9 part (a)
 
 // var userColorStart = prompt("Enter a color which u want at the begning of the array");
 // colorName.unshift(userColorStart);
 
-// part (b)
+// Q#9 part (b)
 
 // var userColorEnd = prompt("Enter a color which u want at the end of the array");
 // colorName.push(userColorEnd);
 // console.log(colorName);
 
-// part (c)
+// Q#9 part (c)
 
 // colorName.unshift("golden","aqua")
 // console.log(colorName);
 
-// part (d)
+// Q#9 part (d)
 
 // var deleteFirst = ["golden","aqua","bluevoilet","grey","purple"]
 // deleteFirst.shift();
 // console.log(deleteFirst);
 
-// part (e)
+// Q#9 part (e)
 
 // var arrColors = ["blue","white","gray","brown","green"];
 // arrColors.pop()
 // console.log(arrColors);
 
-// part (f)
+// Q#9 part (f)
 
 // var colors = ["green","yellow","orangered","aqua"];
 // console.log(colors); 
@@ -87,7 +84,7 @@
 // colors.splice(addIndexColor,0,newColor);
 // console.log(colors); 
 
-// part (g)
+// Q#9 part (g)
 
 // var color = ["red","orange","purple","gray","black","pink"];
 // console.log(color);
@@ -96,27 +93,35 @@
 // color.splice(deleteIndex,deleteCount); 
 // console.log(color);
 
+ // Q#10
+ 
 // var studentScore = [320,230,90,50,480,120];
 // studentScore.sort(function(a,b){
 //     return a-b;
 // })
 // console.log(studentScore);
 
+  // Q#10
 
 // var stuScore = [320,230,480,120];
 // stuScore.sort();
 // console.log(stuScore);
 
+  // Q#11
+
 // var cityNames = ["Karachi","Islamabad","Lahore","Multan","Quetta","Peshawar","Faisalabad"]
 // console.log(cityNames);
-// var selectedCities =cityNames.slice(1,4);
+// var selectedCities = cityNames.slice(1,4);
 // console.log(selectedCities);
+
+  // Q#12
 
 // var array = ["This","is","my","cat"];
 // console.log(array);
 // var result = array.join(" ");
 // console.log(result);
 
+  // Q#13 (FIFO-First In First Out)
 
     //     var arr = ["keyboard","mouse","Printer","monitor"];
     //     document.writeln("<b>Devices:<br/>",arr.join(", ") ,"</b><br/>");
@@ -126,17 +131,17 @@
     // document.writeln("<b>Out: <br> ",arr.shift(),"</b><br>");
     // document.writeln("<b>Out: <br> ",arr.shift(),"</b><br>");
 
+    // Q#14  (Last In First Out)
 
-        // var device = ["keyboard","mouse","Printer","monitor"];
-        // document.writeln("<b>Devices:<br/>",device.join(", ") ,"</b><br/>");
-
+    // var device = ["keyboard","mouse","Printer","monitor"];
+    // document.writeln("<b>Devices:<br/>",device.join(", ") ,"</b><br/>");
     // document.writeln("<b>Out: <br> ",device.pop(),"</b><br>");
     // document.writeln("<b>Out: <br> ",device.pop(),"</b><br>");
     // document.writeln("<b>Out: <br> ",device.pop(),"</b><br>");
     // document.writeln("<b>Out: <br> ",device.pop(),"</b><br>");
 
+    // Q#15
 
-// var phoneCompany = ["Apple","Samsung","Motorola","Nokia","Sony","Haier"];
-
-// document.writeln("<select name='PhoneManufactor' id='phone'>,<option selected disabled >Select your phone:</option>,<option value='Apple'>"+phoneCompany.shift()+"</option>,<option value='Samsung'>"+phoneCompany.shift()+"</option>,<option value='Motorola'>"+phoneCompany.shift()+"</option>,<option value='Nokia'>"+phoneCompany.shift()+"</option>, <option value='sony'>"+phoneCompany.shift()+"</option>,<option value='Haier'>"+phoneCompany.shift()+"</option></select>");
+var phoneCompany = ["Apple","Samsung","Motorola","Nokia","Sony","Haier"];
+document.writeln("<select name='PhoneManufactor' id='phone'>,<option selected disabled >Select your phone:</option>,<option value='Apple'>"+phoneCompany.shift()+"</option>,<option value='Samsung'>"+phoneCompany.shift()+"</option>,<option value='Motorola'>"+phoneCompany.shift()+"</option>,<option value='Nokia'>"+phoneCompany.shift()+"</option>, <option value='sony'>"+phoneCompany.shift()+"</option>,<option value='Haier'>"+phoneCompany.shift()+"</option></select>");
 
